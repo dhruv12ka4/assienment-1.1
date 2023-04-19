5ce80f6f88a5af5eac1e9da34f7b12f398260891
dhruv12ka4 <125910898+dhruv12ka4@users.noreply.github.com>
2023-04-19 15:27:43 +0530

    Add files via upload

---
+++ assienment 1.1.py
@@ -0,0 +1,103 @@
+1. What is the type of the following: 1.0
+A) float B) int C) str
+
+Answer: float
+
+2. What is the result of the following code segment: int(12.3)
+A) 12.3 B) 12 C) 13
+
+Answer: 12
+
+3. What is the result of the following code segment: int(True)
+A) 1 B) 0 C) error
+
+Answer: 1
+
+4. What code would turn the string “1” into an integer?
+A)str(1) B) int("1") C) float("1")
+
+Answer: int("1")
+
+5. Explain the //, %, and ** operators in Python.
+Ans: The // operator performs floor division. It will return the integer part of the result on division.
+
+7//2
+
+3
+
+6. What do you know about relational operators in Python.
+Ans : < (less than)
+
+(Greater than)
+<= (less than or equal to)
+
+= (Greater than or equal to)
+
+== (equal to) != (not equal to)
+
+7. What are assignment operators in Python?
+Ans : = (assign) += (add and assign) -= (subtract and assign) = (multiply and assign) /= (divide and assign) %= (modulus and assign) *= (exponent and assign) //= (floor-divide and assign)
+
+8. What are logical operators in Python?
+Ans: We have three logical operators: and, or, not.
+
+9. What is pass in Python?
+Ans: Pass means, no-operation Python statement, or in other words it is a place holder in compound statement, where there should be a blank left and nothing has to be written there.
+
+10. What Does The Continue Do In Python?
+Ans: The continue is a jump statement in Python which moves the control to execute the next iteration in a loop leaving all the remaining instructions in the block unexecuted.
+
+The continue statement is applicable for both the “while” and “for” loops.
+
+11. When Should You Use The “Break” In Python?
+Ans: Python provides a break statement to exit from a loop. Whenever the break hits in the code, the control of the program immediately exits from the body of the loop.
+
+The break statement in a nested loop causes the control to exit from the inner iterative block.
+
+12. What Is The Difference Between Pass And Continue In Python?
+Ans: The continue statement makes the loop to resume from the next iteration.
+
+On the contrary, the pass statement instructs to do nothing, and the remainder of the code executes as usual.
+
+13. What Is A Built-In Function That Python Uses To Iterate Over A Number Sequence?
+ans: Range() generates a list of numbers, which is used to iterate over for loops.
+
+The range() function accompanies two sets of parameters.
+
+range(stop) stop: It is the no. of integers to generate and starts from zero. eg. range(3) == [0, 1, 2]. range([start], stop[, step]) Start: It is the starting no. of the sequence. Stop: It specifies the upper limit of the sequence. Step: It is the incrementing factor for generating the sequence.
+
+14. How does For loop and While loop differ in Python and when do you choose to use them?
+ans: For loop is generally used to iterate through the elements of various collection types such as List, Tuple, Set, and Dictionary.
+
+While loop is the actual looping feature that is used in any other programming language. This is how Python differs in handling loops from the other programming languages.
+
+15. What are the advantages of using a for loop in Python?
+Ans: A for loop is a type of loop that executes a set of instructions repeatedly. It uses the following syntax: for x in range(y): do something
+
+This type of loop has the following properties:
+
+For loops are good because they can make your code more readable and easier to follow. They also make sure that certain instructions are executed at least one time.
+
+16. What are the disadvantages of using a for loop in Python?
+Ans: The for-loop is one of the most basic constructs in programming. However, this construct has its own disadvantages.
+
+If you don't know the upper-bound of your loop, you have to keep track of it by hand or break out of your loop early. You can't use a break statement to escape from a nested loop.
+
+17. What is lambda in Python?
+Ans: anonymous function means that a function is without a name. As we already know that def keyword is used to define the normal functions and the lambda keyword is used to create anonymous functions.
+
+ lambda arguments: expression
+18. Mention what are the rules for local and global variables in Python?
+Ans: Local variables: If a variable is assigned a new value anywhere within the function's body, it's assumed to be local.
+
+Global variables: Those variables that are only referenced inside a function are implicitly global.
+
+19. What Is A Function In Python Programming?
+Ans: A function is an object which represents a block of code and is a reusable entity. It brings modularity to a program and a higher degree of code reusability.
+
+Python has given us many built-in functions such as print() and provides the ability to create user-defined functions.
+
+20. What Is The Return Keyword Used For In Python?
+Ans: The purpose of a function is to receive the inputs and return some output.
+
+The return is a Python statement which we can use in a function for sending a value back to its caller.
